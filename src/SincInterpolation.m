94d7265b4f0e91e7353a5844a631b97966aa1542
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-05-20 17:28:11 +0530

    format modified

--- src/SincInterpolation.m
+++ src/SincInterpolation.m
@@ -4,11 +4,10 @@ Dt=0.001;
 t1=0:Dt:1;
 n=0:20;
 nt=n*t;
-theta1=0;
 
+theta1=0;
 x1=cos(20*pi*nt+theta1);
 y1=x1*sinc(f*(ones(length(n),1)*t1-nt'*ones(1,length(t1))));
-
 subplot(5,1,1);
 plot(t1,y1,'LINEWIDTH',1.5);
 hold on;
@@ -17,11 +16,10 @@ axis([0 1 -1.2 1.2]);
 xlabel('t in sec');
 ylabel('amplitude');
 title('sinc interpolation for theta=0');
-theta2=pi/6;
 
+theta2=pi/6;
 x2=cos(20*pi*nt+theta2);
 y2=x2*sinc(f*(ones(length(n),1)*t1-nt'*ones(1,length(t1))));
-
 subplot(5,1,2);
 plot(t1,y2,'LINEWIDTH',1.5);
 hold on;
@@ -30,11 +28,10 @@ axis([0 1 -1.2 1.2]);
 xlabel('t in sec');
 ylabel('amplitude');
 title('sinc interpolation for theta=pi/6');
-theta3=pi/4;
 
+theta3=pi/4;
 x3=cos(20*pi*nt+theta3);
 y3=x3*sinc(f*(ones(length(n),1)*t1-nt'*ones(1,length(t1))));
-
 subplot(5,1,3);
 plot(t1,y3,'LINEWIDTH',1.5);
 hold on;
@@ -43,11 +40,10 @@ axis([0 1 -1.2 1.2]);
 xlabel('t in sec');
 ylabel('amplitude');
 title('sinc interpolation for theta=pi/4');
-theta4=pi/3;
 
+theta4=pi/3;
 x4=cos(20*pi*nt+theta4);
 y4=x4*sinc(f*(ones(length(n),1)*t1-nt'*ones(1,length(t1))));
-
 subplot(5,1,4);
 plot(t1,y4,'LINEWIDTH',1.5);
 hold on;
@@ -56,11 +52,10 @@ axis([0 1 -1.2 1.2]);
 xlabel('t in sec');
 ylabel('amplitude');
 title('sinc interpolation for theta=pi/3');
-theta5=pi/2;
 
+theta5=pi/2;
 x5=cos(20*pi*nt+theta5);
 y5=x5*sinc(f*(ones(length(n),1)*t1-nt'*ones(1,length(t1))));
-
 subplot(5,1,5);
 plot(t1,y5,'LINEWIDTH',1.5);
 hold on;
--- src/SplineInterpolation.m
+++ src/SplineInterpolation.m
@@ -3,11 +3,10 @@ Dt=0.001;
 t1=0:Dt:1;
 n=0:20;
 nt=n*t;
-theta1=0;
 
+theta1=0;
 x1=cos(20*pi*nt+theta1);
 y=spline(nt,x1,t1);
-
 subplot(5,1,1);
 plot(t1,y,'LINEWIDTH',1.5);
 axis([0 1 -1.2 1.2]);
@@ -18,10 +17,8 @@ title('spline interpolation for theta=0');
 ylabel('amplitude');
 
 theta2=pi/6;
-
 x2=cos(20*pi*nt+theta2);
 y1=spline(nt,x2,t1);
-
 subplot(5,1,2);
 plot(t1,y1,'LINEWIDTH',1.5);
 axis([0 1 -1.2 1.2]);
@@ -32,10 +29,8 @@ title('spline interpolation for theta=pi/6');
 ylabel('amplitude');
 
 theta3=pi/4;
-
 x3=cos(20*pi*nt+theta3);
 y2=spline(nt,x3,t1);
-
 subplot(5,1,3);
 plot(t1,y2,'LINEWIDTH',1.5);
 axis([0 1 -1.2 1.2]);
@@ -46,10 +41,8 @@ title('spline interpolation for theta=pi/4');
 ylabel('amplitude');
 
 theta4=pi/3;
-
 x4=cos(20*pi*nt+theta4);
 y3=spline(nt,x4,t1);
-
 subplot(5,1,4);
 plot(t1,y3,'linewidth',1.5);
 axis([0 1 -1.2 1.2]);
@@ -60,10 +53,8 @@ title('spline interpolation for theta=pi/3');
 ylabel('amplitude');
 
 theta5=pi/2;
-
 x5=cos(20*pi*nt+theta5);
 y4=spline(nt,x5,t1);
-
 subplot(5,1,5);
 plot(t1,y4,'LINEWIDTH',1.5);
 axis([0 1 -1.2 1.2]);
